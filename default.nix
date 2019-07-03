45bab3174210cb6f6fd5be23a404441522531364
Timo Kaufmann <timokau@zoho.com>
2019-07-03 21:06:07 +0200

    Add nix-shell file

---
+++ default.nix
@@ -0,0 +1,34 @@
+let
+  # pkgs = import <nixpkgs> {};
+  pkgs = let
+    nixpkgs-rev = "73392e79aa62e406683d6a732eb4f4101f4732be"; # nixos-unstable on 2019-07-03
+  in import (builtins.fetchTarball {
+    name = "nixpkgs-${nixpkgs-rev}";
+    url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
+    # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
+  }) {};
+  inherit (pkgs) lib;
+in pkgs.mkShell {
+  buildInputs = [
+    (pkgs.python3.withPackages(ps: with ps; [
+      # use my forked version of baselines
+      # (https://github.com/openai/baselines/pull/931 etc.)
+      (baselines.overrideAttrs (attrs: {
+        # src = lib.cleanSource /home/timo/repos/baselines; # for quick experimenting
+        src = pkgs.fetchFromGitHub {
+          owner = "timokau";
+          repo = "baselines";
+          sha256 = "090pnngl25z2hnkbkja5r6w7p0rvffd58i6ms2lfq3pj8rlg52rp";
+          rev = "80f81380649a6e5685734cf74e615eb815fe2861";
+        };
+      }))
+      graph_nets
+      matplotlib
+      pydot
+    ]))
+  ];
+  shellHook = ''
+    export OPENAI_LOG_FORMAT=stdout,csv,tensorboard
+    export OPENAI_LOGDIR=$PWD/logs/$(date -Is)
+  '';
+}
