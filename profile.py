3bc04a115151337bc6a14b771abfa33978373f85
Timo Kaufmann <timokau@zoho.com>
2019-07-09 22:52:44 +0200

    Continuously generate callgraphs during profiling

--- profile.py
+++ profile.py
@@ -7,35 +7,40 @@ from pycallgraph.output import GraphvizOutput
 from generator import random_embedding, get_random_action
 
 
-def main(time_seconds=60, rand=np.random):
+def main(rand, pcg):
     """Keep randomly exploring embeddings"""
-    total_before = time.time()
     while True:
         action_list = []
         before = time.time()
         embedding = random_embedding(rand)
         while True:
-            if time.time() - total_before > time_seconds:
-                return
-            action = get_random_action(embedding, rand)
+            action = get_random_action(embedding, rand=rand)
             if action is None:
                 break
             action_list.append(action)
+            pcg.start(reset=False)
             embedding.take_action(*action)
+            pcg.stop()
             elapsed_ms = round((time.time() - before) * 1000, 2)
+            if elapsed_ms > 10000:
+                pcg.done()
+            if action is None:
+                break
         actions = len(action_list)
         per_action = round(elapsed_ms / actions, 2)
         print(f"{elapsed_ms}ms ({actions}, {per_action}ms)")
+        # import sys
+        # sys.exit(1)
 
 
-def profile(time_seconds=60, rand=np.random):
+def profile(rand=np.random):
     """Profile the embedding"""
     config = Config()
     config.trace_filter = GlobbingFilter(exclude=["pycallgraph.*"])
     graphviz = GraphvizOutput(output_file=f"pc.png")
-    with PyCallGraph(output=graphviz, config=config):
-        main(time_seconds, rand)
+    pcg = PyCallGraph(output=graphviz, config=config)
+    main(rand, pcg)
 
 
 if __name__ == "__main__":
-    profile(60)
+    profile(rand=np.random.RandomState(42))
