8cbae81556f4230a6f7b70c83590d93db3bf44a1
Timo Kaufmann <timokau@zoho.com>
2019-07-14 16:45:04 +0200

    Fix reading block count from argv

--- experiments/node_block_count.py
+++ experiments/node_block_count.py
@@ -10,5 +10,5 @@ ARGS = DEFAULT
 ARGS["experiment_name"] = f"early_exit_{sys.argv[1]}_{sys.argv[2]}"
 ARGS["generator_args"]["num_sources_dist"] = lambda _: 1
 ARGS["generator_args"]["interm_nodes_dist"] = lambda _: int(sys.argv[1]) - 2
-ARGS["generator_args"]["interm_blocks_dist"] = lambda _: int(sys.argv[1]) - 2
+ARGS["generator_args"]["interm_blocks_dist"] = lambda _: int(sys.argv[2]) - 2
 run_training(**ARGS)
