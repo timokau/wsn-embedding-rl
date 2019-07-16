f549bf39953b0d03519db1685b22f02b59a7b01d
Timo Kaufmann <timokau@zoho.com>
2019-07-16 14:46:57 +0200

    Make feature test embedding more interesting

--- features_test.py
+++ features_test.py
@@ -1,5 +1,6 @@
 """Tests the feature extraction"""
 
+import numpy as np
 from infrastructure import InfrastructureNetwork
 from overlay import OverlayNetwork
 from embedding import PartialEmbedding, ENode
@@ -11,17 +12,29 @@ def test_features():
     """Tests feature extractions on some hand-verified examples"""
     infra = InfrastructureNetwork()
 
-    nso1 = infra.add_source(name="nso1", pos=(0, 0), transmit_power_dbm=30)
-    nso2 = infra.add_source(name="nso2", pos=(0, 1), transmit_power_dbm=30)
-    nsi = infra.set_sink(name="nsi", pos=(2, 0), transmit_power_dbm=30)
+    nso1 = infra.add_source(
+        name="nso1", pos=(0, 0), transmit_power_dbm=30, capacity=1.5
+    )
+    nso2 = infra.add_source(
+        name="nso2", pos=(0, 1), transmit_power_dbm=30, capacity=2.8
+    )
+    nsi = infra.set_sink(
+        name="nsi", pos=(2, 0), transmit_power_dbm=30, capacity=np.infty
+    )
+    ninterm = infra.add_intermediate(
+        name="ninterm", pos=(0, 1), transmit_power_dbm=30, capacity=0
+    )
 
     overlay = OverlayNetwork()
-    bso1 = overlay.add_source(name="bso1", datarate=5, requirement=0)
-    bso2 = overlay.add_source(name="bso2", datarate=5, requirement=0)
-    bin_ = overlay.add_intermediate(name="bin", datarate=5, requirement=0)
-    bsi = overlay.set_sink(name="bsi", datarate=5, requirement=0)
-    overlay.add_link(bso1, bin_)
-    overlay.add_link(bin_, bsi)
+    bso1 = overlay.add_source(name="bso1", datarate=5, requirement=1)
+    bso2 = overlay.add_source(name="bso2", datarate=5, requirement=2)
+    bin1 = overlay.add_intermediate(name="bin1", datarate=5, requirement=3)
+    bin2 = overlay.add_intermediate(name="bin2", datarate=5, requirement=0)
+    bsi = overlay.set_sink(name="bsi", datarate=5, requirement=4)
+    overlay.add_link(bso1, bin1)
+    overlay.add_link(bso1, bin2)
+    overlay.add_link(bin1, bsi)
+    overlay.add_link(bin2, bsi)
     overlay.add_link(bso2, bsi)
 
     embedding = PartialEmbedding(
@@ -30,9 +43,13 @@ def test_features():
 
     eso1 = ENode(bso1, nso1)
     eso2 = ENode(bso2, nso2)
-    ein = ENode(bin_, nsi)
     esi = ENode(bsi, nsi)
-    erelay = ENode(bso1, nso2, bin_)
+    erelay = ENode(bso1, ninterm, bin1)
+    ein = ENode(bin1, nsi)
+    erelay_unchosen = ENode(bso2, ninterm, bsi)
+
+    assert embedding.take_action(eso1, erelay, 0)
+    assert embedding.take_action(erelay, ein, 1)
 
     feature_dict = features_by_name()
 
@@ -44,6 +61,7 @@ def test_features():
     assert node_feature("pos", ein) == (2, 0)  # pos of nsi
 
     assert node_feature("relay", erelay) == (1.0,)
+    assert node_feature("relay", erelay_unchosen) == (1.0,)
     assert node_feature("relay", eso1) == (0.0,)
     assert node_feature("relay", ein) == (0.0,)
 
